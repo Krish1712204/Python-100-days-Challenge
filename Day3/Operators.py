6bae8df4ec64a6cd187f4ef29c51d88999fe9dd6
Krishna Shelar <168703616+Krish1712204@users.noreply.github.com>
2024-08-13 07:12:00 +0530

    Add files via upload
    Variables and Operators

---
+++ Day3/Operators.py
@@ -0,0 +1,35 @@
+# Arithmathic operators
+# +,-,*,/
+a=3
+b=8
+print(a+b)
+print(a)
+
+print(a*b)
+
+# Assignment operators
+# =,+=,-=,*=,/=
+a=4+2  #Assign 4+2 in a
+print(a)
+
+b=8
+b+=4 #Increment the value of b by 3 and assign it to b
+b-=4 #Decrement the value of b by 3 and assign it to b
+print(b)
+
+#Comparison operators
+
+f=5==4
+print (f)
+
+d=5>=4
+print(d)
+
+g=6<4
+print(g)
+
+h=7!=7
+print(h)
+
+j=7==7
+print(j)
---
+++ Day3/variable.py
@@ -0,0 +1,12 @@
+a=2  #Integer
+b=4.4 #Floating point number
+
+print(a)
+print(b)
+
+_krish=7
+print(_krish)
+
+a="tejas"  #String
+print(a)
+
