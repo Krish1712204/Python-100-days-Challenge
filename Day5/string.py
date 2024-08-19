6ef8dce54802a2075abff271efbfd961fe8ad5b8
Krishna Shelar <168703616+Krish1712204@users.noreply.github.com>
2024-08-19 23:32:37 +0530

    String in Python

---
+++ Day5/string.py
@@ -0,0 +1,20 @@
+# name="Krishna"
+# print(name)
+
+# Length of string
+# print(len(name))
+
+
+#String Slicing
+#First index is included and last index is excluded -> (0:3) 
+
+# name="Krishna"
+
+# nameshort=name[0:2]
+
+# print(nameshort)  #It will display op as -krish
+
+# character=name[3]
+# print(character)
+
+
