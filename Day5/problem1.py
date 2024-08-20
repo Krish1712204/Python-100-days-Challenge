9891b2e849b4f934249373d2a12e7a500b195caa
Krishna Shelar <168703616+Krish1712204@users.noreply.github.com>
2024-08-20 08:06:51 +0530

    Simple Problems on strings

---
+++ Day5/problem1.py
@@ -0,0 +1,6 @@
+#Write a python program to display a user enter name followed by good afternoon using input()
+
+name=input("Enter name:")
+print("Good AfterNoon",name)
+
+print(f"Good afterNoon {name}")
---
+++ Day5/problem3.py
@@ -0,0 +1,3 @@
+#Write a python program to detect double space in string
+name="my name is  Krishna"
+print(name.find("  "))
